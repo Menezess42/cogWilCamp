e45aff150c5cb803bc3e34e10032b088a1f9be37
Menezess42 <arielmenezess@hotmail.com>
2026-07-10 16:00:49 -0300

    config: configurating enviroment
    configurating devenv dependencies and codeBase simples schema

--- devenv.nix
+++ devenv.nix
@@ -1,45 +1,38 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+    packages = [
+        pkgs.pyright
+    ];
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+    languages.python = {
+        enable = true;
+        package = pkgs.python313.withPackages (p: with p; [
+            numpy
+            pandas
+            pytest
+            ipykernel
+            ipython
+            nbformat
+            pyqt5
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+            # NVIM
+            pip
+            jedi
+            jedi-language-server
+            black
+            flake8
+            sentinel
+            python-lsp-server
+            virtualenv
+            pyflakes
+            isort
+            debugpy
+        ]);
+        venv.enable = true;
+    };
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+    enterShell = ''
+      echo "$(python --version) — venv ativo"
+    '';
 }
